2ff71c211315bbe94ba3db8b1e529df7ca07c58e
JORDI RUF VILCHES <jordiruf@gmail.com>
2021-05-19 00:54:55 +0200

    Update

--- R/tools.R
+++ R/tools.R
@@ -1,3 +1,12 @@
+#' Combinatoria
+#' 
+#' @param x Vector de caracteres.
+#' @return Vector con la combinación de elementos de \code{x}.
+#' @export
+#' @examples 
+#' vector <- c("A", "B", "C")
+#' combina(vector)
+ 
 combina <- function(x) {
   vec <- c()
   for(i in 1:(length(x)-1)) {
@@ -8,6 +17,16 @@ combina <- function(x) {
   }
   return(vec)
 }
+
+#' Combinatoria 2
+#'
+#' @param x Vector de caracteres.
+#' @return Vector con la combinación de elementos de \code{x}. Cada elemento de
+#'   \code{x} estará introducido en un paréntesis.
+#' @export
+#' @examples
+#' vector <- c("A-B", "A-C", "B-C")
+#' combina_int(vector)
 
 combina_int <- function(x) {
   vec <- c()
